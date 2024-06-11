c4e8afa8bbf507b543b78f8c154f6f99594ad9b4
Callum Jones <callum.o.jones+git@protonmail.com>
2024-06-11 20:32:43 +1000

    Add blob loading code for backend

--- src/data/importer.ts
+++ src/data/importer.ts
@@ -1,19 +1,48 @@
-import { BaseDirectory, FsDirOptions, readDir } from "@tauri-apps/api/fs"
+import { BaseDirectory, FileEntry, FsDirOptions, readDir } from "@tauri-apps/api/fs"
 import { homeDir, join } from "@tauri-apps/api/path";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
+
+/**
+ * Recursively scan from a root file entry, and return a list of paths to every
+ * sub-file contained within it that has no children of its own
+ * @param rootFile
+ * @returns
+ */
+const filesInDirTree = (rootFile: FileEntry): string[] => {
+  // Base case - we find a file, we just return that path
+  if (!rootFile.children) {
+    return [rootFile.path];
+  }
+
+  // This is a folder, so we need to scan its subfolders
+  let res: string[] = [];
+  rootFile.children.forEach(childFile => {
+    const tmp = filesInDirTree(childFile);
+    res = res.concat(tmp);
+  });
+  // console.log(res);
+  return res;
+}
 
 /**
  * Scan a folder recursively for any music files within it.
  */
 export const scanFolder = (rootDir: string) => {
+
   const readOptions: FsDirOptions = {
     dir: BaseDirectory.Home,
     recursive: true,
   };
+
   const dirPromise = readDir(rootDir, readOptions);
+
   dirPromise
     .then(data => {
       console.log(data);
+      data.forEach(childDir => {
+        const subFiles = filesInDirTree(childDir);
+        console.log(subFiles);
+      })
     })
     .catch(err => {
       console.log(err);
@@ -30,4 +59,23 @@ export const playSongFromURI = (onUriLoad: (uri: string) => void) => {
     .catch(err => {
       console.log(err);
     });
+}
+
+export const playSongFromAbsPath = (songPath: string) => {
+  const res = fetch(convertFileSrc(songPath))
+    .then(res => {
+      const tmp = res.blob()
+        .then(blob => {
+          const url = URL.createObjectURL(blob);
+          return url;
+        })
+        .catch(err => {
+          console.log(err)
+          return undefined;
+        });
+
+      return tmp;
+    });
+
+    return res;
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,6 +2,7 @@ import MusicFooter from "../components/MusicFooter";
 import MusicGrid from "../components/MusicGrid";
 import SideBar from "../components/SideBar";
 import SongList from "../components/SongList";
+import { playSongFromAbsPath, scanFolder } from "../data/importer";
 
 // FIXME consolidate music data into a single
 export interface HomeScreenProps {
@@ -21,7 +22,28 @@ export default function HomeScreen ({ toggleAudioPlaying, isPlaying, setSongPos,
       <div className="flex flex-row flex-grow h-1 flex-shrink">
         <SideBar />
         {/* <MusicGrid changeAudioSrc={changeAudioSrc} /> */}
-        <div className="basis-full flex-grow-0 min-w-0">
+        <div className="basis-full flex-grow-0 min-w-0 relative">
+          <button
+            className="bg-blue-800 p-4 absolute bottom-4 right-8 rounded-lg text-blue-50 font-bold shadow-md shadow-green-950"
+            onClick={() => scanFolder('Music')}
+          >
+            Scan
+          </button>
+          <button
+            className="bg-blue-800 p-4 absolute bottom-20 right-8 rounded-lg text-blue-50 font-bold shadow-md shadow-green-950"
+            onClick={() => {
+              playSongFromAbsPath('C:/Users/Callum/Music/MusicBee/Artist Pictures/Thumb/Alice Phoebe Lou.jpg')
+                .then(data => {
+                  if (!data) return;
+
+                  console.log(data);
+                  changeAudioSrc(data);
+                })
+                .catch(data => console.error(data));
+            }}
+          >
+            Load Test Song
+          </button>
           <SongList />
         </div>
       </div>
