6bdb2045d11b8743a61fe1a142e68aef88811480
Callum Jones <callum.o.jones+git@protonmail.com>
2024-09-06 20:46:05 +1000

    Strip back rust backend to basics

---
+++ notes.md
@@ -0,0 +1,17 @@
+# Types
+
+Collection
+  - List of songs
+  - List of albums
+
+Songs:
+  - Path of music file
+  - Tags attached to that file
+
+Tags:
+  - Data possibly written in a song's metadata
+
+Album:
+  - Title of the album
+  - Artwork
+  - References to songs in the album
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use album::Album;
-use app::App;
-use song::Song;
-use tauri::State;
 
 mod app;
 mod collection;
-mod data;
-mod song;
-mod album;
 
 fn main() {
-    let app = App::new(vec![String::from("C:/Users/Callum/Music/")]);
+    // let app = App::new(vec![String::from("C:/Users/Callum/Music/")]);
 }
