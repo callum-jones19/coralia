4634e571bf308b93df0949c0a05b229017b4e797
Callum Jones <callum.o.jones+git@protonmail.com>
2024-09-06 20:33:42 +1000

    Remove tauri-specific code

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -4,37 +4,15 @@ use crate::{collection::Collection, song::Song};
 
 #[derive(Debug)]
 pub struct App {
-    pub collection: Mutex<Collection>,
+    pub collection: Collection,
 }
 
 impl App {
     pub fn new(root_dirs: Vec<String>) -> Self {
         let collection = Collection::new_from_root_dirs(root_dirs);
 
-        println!("{:?}", collection.get_all_albums());
-
         App {
-            collection: Mutex::from(collection),
+            collection: collection,
         }
-    }
-
-    pub fn filter_songs_by_title(&self, song_name: String) -> Vec<Song> {
-        self.collection
-            .lock()
-            .unwrap()
-            .filter_songs_by_name_ignore_case(song_name)
-    }
-
-    pub fn filter_songs_by_album(&self, album: String) -> Vec<Song> {
-        self.collection
-            .lock()
-            .unwrap()
-            .filter_songs_by_album_ignore_case(album)
-    }
-
-    pub fn get_all_songs(&self) -> Vec<Song> {
-        let mut res = self.collection.lock().unwrap().get_all_songs().clone();
-        res.sort_by(|a, b| a.tags.album.cmp(&b.tags.album));
-        res
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,15 +14,4 @@ mod album;
 
 fn main() {
     let app = App::new(vec![String::from("C:/Users/Callum/Music/")]);
-
-    tauri::Builder::default()
-        .manage(app)
-        .invoke_handler(tauri::generate_handler![
-            filter_songs_by_title,
-            get_all_songs,
-            filter_songs_by_album,
-            get_all_albums
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
 }
