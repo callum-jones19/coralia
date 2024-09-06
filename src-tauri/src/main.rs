0edf8c8bdf166ffa75c7e2a2ff3628372df73c4d
Callum Jones <callum.o.jones+git@protonmail.com>
2024-09-06 20:31:14 +1000

    Remove old IPC API calls from main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -26,27 +26,3 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
-#[tauri::command(async)]
-fn filter_songs_by_title(title_filter: String, state: State<App>) -> Vec<Song> {
-    println!("1");
-    state.filter_songs_by_title(title_filter)
-}
-
-#[tauri::command(async)]
-fn filter_songs_by_album(album: String, state: State<App>) -> Vec<Song> {
-    println!("7");
-    state.filter_songs_by_album(album)
-}
-
-#[tauri::command(async)]
-fn get_all_songs(state: State<App>) -> Vec<Song> {
-    println!("2");
-    state.get_all_songs()
-}
-
-#[tauri::command(async)]
-fn get_all_albums(state: State<App>) -> Vec<Album> {
-
-    state.collection.lock().unwrap().get_all_albums()
-}
