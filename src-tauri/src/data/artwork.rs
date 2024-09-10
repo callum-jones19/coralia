75aebac43cc76cff8e49ef56fda84262ce62eff0
Callum Jones <callum.o.jones+git@protonmail.com>
2024-09-10 15:58:06 +1000

    Add safe getter for first available artwork

--- src-tauri/src/data/artwork.rs
+++ src-tauri/src/data/artwork.rs
@@ -2,6 +2,12 @@ use std::path::Path;
 
 use serde::{Deserialize, Serialize};
 
+/// Represents the artwork we expect to possibly see associated with a music file
+/// By default prefers folder_album_art as default, and uses embedded artwork as
+/// the fallback
+///
+/// TODO In future, we will have the user define which artwork they would prefer
+/// to use as the default
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Artwork {
     cached_embedded_art: Option<Box<Path>>,
@@ -22,5 +28,15 @@ impl Artwork {
 
     pub fn has_art(&self) -> bool {
         self.cached_embedded_art.is_some() || self.folder_album_art.is_some()
+    }
+
+    pub fn get_artwork(&self) -> Option<&Path> {
+        if let Some(art) = &self.folder_album_art {
+            Some(art.as_ref())
+        } else if let Some(art) = &self.cached_embedded_art {
+            Some(art.as_ref())
+        } else {
+            None
+        }
     }
 }
