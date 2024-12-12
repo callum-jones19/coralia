2b514a6541e09b7bbf15ecc6ad9d861fc8488d64
Callum Jones <callum.o.jones+git@protonmail.com>
2024-12-12 22:42:47 +1100

    Upgrade React Router to v7

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 
