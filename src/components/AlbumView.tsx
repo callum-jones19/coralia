fa8b0c0c8cbfac8f3a479a4b501887e9dfd1b7dd
Callum Jones <callum.o.jones+git@protonmail.com>
2025-01-12 18:41:07 +1100

    Change bg colour of back button on album view

--- src/components/AlbumView.tsx
+++ src/components/AlbumView.tsx
@@ -69,7 +69,7 @@ export default function AlbumView() {
       {album && songs.length > 0 &&
         <div className="flex flex-col min-h-full">
           <Link
-            className="bg-neutral-300 w-fit p-2 ml-3 mt-2 rounded-md"
+            className="w-fit p-2 ml-3 mt-2 rounded-md"
             to="/home/albums"
           >
             <ChevronLeft />
