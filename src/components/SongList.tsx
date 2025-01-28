c6e50041afa92c1b66cb358b7db3a69695ccc35c
Callum Jones <callum.o.jones+git@protonmail.com>
2025-01-29 10:10:17 +1100

    Fix padding in Song List

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -67,8 +67,8 @@ export default function SongList() {
 
   return (
     <div className="basis-1/2 flex-grow h-full flex flex-col">
-      <div className="font-bold flex flex-row justify-between text-start gap-4 pt-2 pb-2 bg-neutral-900 border-b-2 border-neutral-200 border-solid pl-2">
-        <p className="basis-1/5">Disk #</p>
+      <div className="font-bold flex flex-row justify-between text-start gap-4 pt-2 pb-2 bg-neutral-900 border-b-2 border-neutral-200 border-solid mr-4">
+        <p className="basis-1/5 pl-2">Disk #</p>
         <p className="basis-1/5">Title</p>
         <p className="basis-1/5">Album</p>
         <p className="basis-1/5">Artist</p>
