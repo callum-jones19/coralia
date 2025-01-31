e749af63ebd900ec3d488fd0030ba69ddcb2fbf7
Callum Jones <callum.o.jones+git@protonmail.com>
2025-01-31 11:42:11 +1100

    Change overflow of library body from auto to hidden

--- src/components/LibraryBody.tsx
+++ src/components/LibraryBody.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function LibraryBody() {
   return (
     <>
-      <div className="bg-neutral-100 basis-1/2 overflow-auto flex-grow rounded-md h-full">
+      <div className="bg-neutral-100 basis-1/2 overflow-hidden flex-grow rounded-md h-full">
         <Outlet />
       </div>
     </>
