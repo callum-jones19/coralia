936eed40200e216bc64617999f7fdfef593902f8
Callum Jones <callum.o.jones+git@protonmail.com>
2025-01-31 11:46:47 +1100

    Replace overflow hide with min-w-0

--- src/components/LibraryBody.tsx
+++ src/components/LibraryBody.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function LibraryBody() {
   return (
     <>
-      <div className="bg-neutral-100 basis-1/2 overflow-hidden flex-grow rounded-md h-full">
+      <div className="bg-neutral-100 basis-1/2 min-w-0 flex-grow rounded-md h-full">
         <Outlet />
       </div>
     </>
