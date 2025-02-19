48419043471ba866ac1987f41c5a876743a48df4
Callum Jones <callum.o.jones+git@protonmail.com>
2025-02-19 21:36:54 +1100

    Improve settings UI

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router";
 import { resetLibraryBackend } from "../api/commands";
+import { ChevronLeft } from "react-feather";
 
 export default function SettingsScreen() {
   const navigate = useNavigate();
@@ -15,14 +16,16 @@ export default function SettingsScreen() {
   };
 
   return (
-    <div className="w-full h-full flex flex-row">
-      <div className="basis-1/4 flex flex-col shadow-md p-2">
-        <h2 className="font-bold text-lg">Settings</h2>
-        <Link to="/home" className=" p-2 rounded-md text-center">
-          Home
-        </Link>
+    <div className="w-full h-full flex flex-row dark:bg-neutral-900 dark:text-white">
+      <div className="basis-60 flex flex-col m-2 gap-4 rounded-md shadow-md p-4 dark:shadow-none dark:bg-neutral-800">
+        <div className="flex flex-row gap-2 items-center">
+          <Link to="/home" className="flex flex-row h-full aspect-square items-center justify-center">
+            <ChevronLeft />
+          </Link>
+          <h2 className="font-bold text-lg">Settings</h2>
+        </div>
       </div>
-      <div className="flex-grow p-2">
+      <div className="flex-grow p-4 m-2 ml-0 shadow-md rounded-md dark:shadow-none dark:bg-neutral-800">
         <button
           className="bg-red-500 p-3 rounded-md text-white font-bold"
           onClick={() => resetLibrary()}
