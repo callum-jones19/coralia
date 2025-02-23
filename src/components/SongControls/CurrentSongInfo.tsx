91642f373a95a9ca68eb74ca42bef6e83557495d
Callum Jones <callum.o.jones+git@protonmail.com>
2025-02-23 18:21:25 +1100

    Improve UI of seekbar song info

--- src/components/SongControls/CurrentSongInfo.tsx
+++ src/components/SongControls/CurrentSongInfo.tsx
@@ -7,18 +7,18 @@ export interface CurrentSongInfoProps {
 export default function CurrentSongInfo({ currentSong }: CurrentSongInfoProps) {
   return (
     <>
-      <div id="music-info" className="flex flex-col text-center flex-grow">
+      <div id="music-info" className="flex flex-col text-center flex-grow h-14">
         <p className="font-bold">
           {currentSong
             ? (currentSong.tags.title ? currentSong.tags.title : "N/A")
-            : "~"}
+            : ""}
         </p>
         <p>
           {currentSong
             ? (currentSong.tags.artist !== null
               ? currentSong.tags.artist
               : "N/A")
-            : "~"}
+            : ""}
         </p>
       </div>
     </>
