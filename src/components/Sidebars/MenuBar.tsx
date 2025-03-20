a6c407243a7049521a98f377117df3cf995f0a28
Callum Jones <callum.o.jones+git@protonmail.com>
2025-03-20 23:57:28 +1100

    Make go back button hard-coded for album page

--- src/components/Sidebars/MenuBar.tsx
+++ src/components/Sidebars/MenuBar.tsx
@@ -21,6 +21,7 @@ export default function MenuBar({ onSearch }: MenuBarProps) {
   const loc = useLocation();
 
   const [activeSection, setActiveSection] = useState<ActiveSection>("Songs");
+  const [canGoBack, setCanGoBack] = useState<boolean>(false);
 
   useEffect(() => {
     console.log(loc);
@@ -29,6 +30,12 @@ export default function MenuBar({ onSearch }: MenuBarProps) {
       setActiveSection("Songs");
     } else if (loc.pathname === "/home/albums") {
       setActiveSection("Albums");
+    }
+
+    if (loc.pathname.startsWith("/home/album/")) {
+      setCanGoBack(true);
+    } else {
+      setCanGoBack(false);
     }
   }, [loc]);
 
@@ -54,7 +61,7 @@ export default function MenuBar({ onSearch }: MenuBarProps) {
   };
 
   const handleClickBack = () => {
-    const t = navigate(-1);
+    const t = navigate("/home/albums");
     if (t) {
       t.catch(e => console.error(e));
     }
@@ -63,23 +70,24 @@ export default function MenuBar({ onSearch }: MenuBarProps) {
   return (
     <>
       <BackgroundCard className="basis-16 lg:basis-52 flex-grow-0 flex-shrink-0 rounded-md py-2 px-2 w-full h-full flex flex-col justify-between overflow-auto">
-        <div className="w-full flex flex-col gap-2 items-start">
+        <div className="w-full flex flex-col gap-2 items-center lg:items-start">
           <div className="w-full border-b border-neutral-600 pb-4">
-            <div className="flex flex-row gap-1 items-center w-full mb-4">
+            <div className="flex flex-row gap-1 items-center w-full mb-4 justify-center">
               <button
-                className="hover:bg-neutral-300 hover:dark:bg-neutral-600 p-1 rounded-md w-8 h-8 inline-flex items-center justify-center"
+                className="disabled:hover:bg-transparent disabled:dark:text-neutral-400 disabled:text-neutral-500 hover:bg-neutral-300 hover:dark:bg-neutral-600 p-1 rounded-md w-8 h-8 inline-flex items-center justify-center"
+                disabled={!canGoBack}
                 onClick={handleClickBack}
               >
                 <ArrowLeft className="h-5/6 w-5/6" />
               </button>
-              <p className="font-semibold">Library</p>
+              <p className="font-semibold hidden lg:block flex-grow">Library</p>
             </div>
             <SearchBar onSearch={onSearch} />
 
           </div>
           {/* <h3 className="font-semibold text-lg hidden ml-2 lg:block">Library</h3> */}
           <button
-            className={`flex flex-row items-center justify-center lg:justify-start gap-2 w-full rounded-md p-2 ${activeSection !== "Songs"
+            className={`flex flex-row items-center justify-center lg:justify-start gap-2 w-full rounded-md mt-2 p-2 ${activeSection !== "Songs"
               ? "hover:bg-neutral-200 hover:dark:bg-neutral-700"
               : "bg-neutral-300 dark:bg-neutral-900"
               }`}
@@ -103,7 +111,7 @@ export default function MenuBar({ onSearch }: MenuBarProps) {
           className="flex flex-row items-center justify-center lg:justify-start gap-2 w-full rounded-md p-2 hover:bg-neutral-300 hover:dark:bg-neutral-700"
           onClick={() => handleClickSettings()}
         >
-          <Settings />
+          <Settings className="h-5 w-5" />
           <p className="hidden lg:block">Settings</p>
         </button>
       </BackgroundCard>
