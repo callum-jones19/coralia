5b9397ca18bcda929a042a6409b4a43eaa005016
Callum Jones <callum.o.jones+git@protonmail.com>
2025-03-21 15:31:27 +1100

    Fix settings sidebar on lg media query
    Closes #16

--- src/components/Settings/SettingsSidebar.tsx
+++ src/components/Settings/SettingsSidebar.tsx
@@ -5,7 +5,7 @@ import BackgroundCard from "../UI/BackgroundCard";
 export default function SettingsSidebar() {
   return (
     <>
-      <BackgroundCard className="basis-16 lg:basis-52 flex-grow-0 flex-shrink-0 rounded-md py-2 px-2 w-full h-full flex flex-col justify-start overflow-auto">
+      <BackgroundCard className="basis-16 lg:basis-52 flex-grow-0 flex-shrink-0 rounded-md py-2 px-2 w-full h-full flex flex-col justify-start items-center overflow-auto">
         <div className="flex flex-row gap-1 items-center w-full mb-4 justify-center border-b border-neutral-600 pb-4">
           <Link
             className="disabled:hover:bg-transparent disabled:dark:text-neutral-400 disabled:text-neutral-500 hover:bg-neutral-300 hover:dark:bg-neutral-600 p-1 rounded-md w-8 h-8 inline-flex items-center justify-center"
@@ -20,20 +20,22 @@ export default function SettingsSidebar() {
           className={({ isActive }) => (`${isActive
             ? "bg-neutral-300 dark:bg-neutral-900"
             : "bg-transparent"
-            } text-start p-2 rounded-md flex items-center gap-3 disabled:text-neutral-300`)}
+            } text-start p-2 rounded-md flex items-center gap-3 disabled:text-neutral-300 justify-center lg:justify-start h-10 w-10 lg:w-full`)}
+          title="Appearance"
         >
           <Sun className="h-5 w-5" />
-          <p>Appearance</p>
+          <p className="hidden lg:block">Appearance</p>
         </NavLink>
         <NavLink
           to="library"
           className={({ isActive }) => (`${isActive
             ? "bg-neutral-300 dark:bg-neutral-900"
             : "bg-transparent"
-            } text-start p-2 rounded-md flex items-center gap-3 disabled:text-neutral-300`)}
+            } text-start p-2 rounded-md flex items-center gap-3 disabled:text-neutral-300 justify-center lg:justify-start h-10 w-10 lg:w-full`)}
+          title="Library"
         >
           <Layers className="h-5 w-5" />
-          <p>Library</p>
+          <p className="hidden lg:block">Library</p>
         </NavLink>
       </BackgroundCard>
     </>
