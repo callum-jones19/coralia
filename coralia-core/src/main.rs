2c045a8b7ce8ed6b89ae4e60e2abc095208fda26
Callum Jones <callum.o.jones+git@protonmail.com>
2025-06-06 17:53:10 +1000

    Begin connecting frontend and backend in core

--- coralia-core/src/main.rs
+++ coralia-core/src/main.rs
@@ -1,5 +1,19 @@
+use std::io;
+
+use coralia_gui::start_gui;
 use coralia_player::PlayerState;
 
-fn main() {
-    let player_state = PlayerState::new();
+#[tokio::main]
+async fn main() {
+    // Initialise the GUI
+    tokio::task::spawn(async {
+        // Initialise the backend state
+        let player_state = PlayerState::new();
+    });
+
+    start_gui();
+    loop {
+        // Handle clicked events
+        todo!()
+    }
 }
--- coralia-gui/src/lib.rs
+++ coralia-gui/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use eframe::egui;
+
+pub fn start_gui() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Coralia",
+        native_options,
+        Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))),
+    ).unwrap();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Default)]
+struct MyEguiApp {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
     }
 }
