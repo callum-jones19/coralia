c04e41c0c5f1c3dfa1ca8c18edde3ff4914770d3
Callum Jones <callum.o.jones+git@protonmail.com>
2025-06-09 01:15:48 +1000

    Finish scaffold of total UI layout

--- coralia-gui/src/lib.rs
+++ coralia-gui/src/lib.rs
@@ -1,9 +1,12 @@
-use iced::alignment::Vertical;
+use iced::alignment::{Horizontal, Vertical};
 use iced::font::Weight;
 use iced::widget::image::Handle;
 use iced::widget::text::Style;
-use iced::widget::{Slider, Text, button, column, container, image, row, text};
-use iced::{Element, Font, Length, Padding};
+use iced::widget::{
+    Container, PaneGrid, Slider, Text, button, column, container, image, row, scrollable, text,
+    vertical_space,
+};
+use iced::{Background, Color, Element, Font, Length, Padding};
 
 pub fn start_gui() -> iced::Result {
     iced::run("Coralia", update, view)
@@ -38,6 +41,31 @@ fn update(root_state: &mut RootState, message: Message) {
 ///
 fn view(root_state: &RootState) -> Element<Message> {
     column![render_body(root_state), render_bottom_panel(root_state)].into()
+    // c.explain(Color::WHITE)
+}
+
+fn render_sidebar(root_state: &RootState) -> Element<Message> {
+    column([
+        button("Albums").on_press(Message::Increment).into(),
+        button("Songs").on_press(Message::Decrement).into(),
+    ])
+    .align_x(Horizontal::Left)
+    .width(Length::Shrink)
+    .height(Length::Fixed(50.0))
+    .into()
+}
+
+fn render_library(root_state: &RootState) -> Element<Message> {
+    let t = (0..=10000).map(|i| text!("{}", i).into());
+
+    scrollable(column(t)).width(Length::Fill).into()
+}
+
+fn render_body(root_state: &RootState) -> Element<Message> {
+    row![render_sidebar(root_state), render_library(root_state)]
+        .height(Length::Fill)
+        .spacing(5)
+        .into()
 }
 
 fn render_bottom_panel(root_state: &RootState) -> Element<Message> {
@@ -56,8 +84,7 @@ fn render_bottom_panel(root_state: &RootState) -> Element<Message> {
         .content_fit(iced::ContentFit::ScaleDown);
     let info_block =
         container(row![cover_img, column![song_title_text, song_artist_text]].spacing(10))
-            .center_y(Length::Fill)
-            .height(Length::Fixed(80.0));
+            .center_y(Length::Fill);
 
     let seekbar = Slider::new(0.0..=100.0, 0.0, Message::ValueChanged);
     let curr_pos = Text::new("00:00");
@@ -71,17 +98,8 @@ fn render_bottom_panel(root_state: &RootState) -> Element<Message> {
     let total_container = row!(info_block, seekbar_block, controls_block)
         .align_y(Vertical::Center)
         .spacing(40)
-        .height(Length::Fixed(60.0))
+        .height(Length::Fixed(90.0))
         .padding(10);
 
     total_container.into()
-}
-
-fn render_body(root_state: &RootState) -> Element<Message> {
-    container("Test")
-        .padding(10)
-        .style(container::rounded_box)
-        .center_x(Length::Fill)
-        .center_y(Length::Fill)
-        .into()
 }
